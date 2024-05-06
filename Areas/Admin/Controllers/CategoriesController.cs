a32c87d45c3f5841e5179afb47d73a8b1c5f487c
bichquyen05 <bichquyen.050403@gmail.com>
2024-05-06 23:50:11 +0700

    [FEATURE] Add category management functionality

---
+++ Areas/Admin/Controllers/CategoriesController.cs
@@ -0,0 +1,132 @@
+﻿using BookShop.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookShop.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("Admin/Categories")]
+    public class CategoriesController : Controller
+    {
+        private readonly DataContext _context;
+        public CategoriesController(DataContext context)
+        {
+            _context = context;
+        }
+        [Route("CategoryList")]
+        public async Task<IActionResult> CategoryList()
+        {
+            var categories = await _context.Categories.ToListAsync();
+            var bookCount = new Dictionary<string, int>();
+            foreach(var item in categories)
+            {
+                int counts = await _context.Books.Where(b => b.CategoryId == item.Id).CountAsync();
+                bookCount.Add(item.Name, counts);
+            }
+            ViewData["bookCounts"] = bookCount;
+            return View(categories);
+        }
+
+        [Route("Create")]
+        [HttpGet]   
+        public IActionResult Create()
+        {
+            return View();
+        }               
+        [Route("Create")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Category category)
+        {
+            var existingTheLoai = await _context.Categories.FirstOrDefaultAsync(t => t.Id != category.Id && t.Name == category.Name);
+            if (existingTheLoai != null)
+            {
+                TempData["Message"] = "This category already exists!";
+            }
+            else
+            {
+                if (ModelState.IsValid)
+                {
+                    _context.Categories.Add(category);
+                    await _context.SaveChangesAsync();
+                    TempData["Message"] = "Create successful!";
+                    return RedirectToAction("CategoryList");
+                }
+            }            
+            return View(category);
+        }
+
+        [Route("Edit")]
+        [HttpGet]
+        public async Task<IActionResult> Edit(int? id)
+        {           
+            var category = await _context.Categories.FindAsync(id);
+            if (category == null)
+            {
+                TempData["Message"] = "Category is not exist!";
+            }
+            return View(category);
+        }
+
+        [Route("Edit")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Category category)
+        {
+            if(id!=category.Id)
+            {
+                TempData["Message"] = "Category is not exist!";
+            }
+            var existingTheLoai = await _context.Categories.FirstOrDefaultAsync(t => t.Id != id && t.Name == category.Name);
+            if(existingTheLoai != null)
+            {
+                TempData["Message"] = "This category already exists!";
+            }
+            else
+            {
+                _context.Entry(category).State = EntityState.Modified;
+                try
+                {
+                    _context.Update(category);
+                    await _context.SaveChangesAsync();
+                    TempData["Message"] = "Update successful!";
+                    return RedirectToAction("CategoryList");
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    TempData["Message"] = "Update Failed!";
+                }
+            }           
+            return View(category);
+        }
+
+        [Route("Delete")]
+        [HttpGet]
+        public async Task<IActionResult> Delete(int id)
+        {
+            // Kiểm tra xem có cuốn sách nào thuộc thể loại đó hay không
+            var categoryHasBooks = await _context.Books.AnyAsync(b => b.CategoryId == id);
+
+            if (categoryHasBooks)
+            {
+                TempData["Message"] = "This category cannot be deleted because there are books in this category!";
+                return RedirectToAction("CategoryList");
+            }
+
+            // Xóa thể loại nếu không có cuốn sách nào thuộc thể loại đó
+            var categoryToDelete = await _context.Categories.FindAsync(id);
+            if (categoryToDelete == null)
+            {
+                TempData["Message"] = "No categories found to delete!";
+                return RedirectToAction("CategoryList");
+            }
+
+            _context.Categories.Remove(categoryToDelete);
+            await _context.SaveChangesAsync();
+
+            TempData["Message"] = "The category has been successfully deleted!";
+            return RedirectToAction("CategoryList");
+        }
+
+    }
+}
--- Models/Category.cs
+++ Models/Category.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace BookShop.Models;
 
 public partial class Category
 {
+    [Key]
     public int Id { get; set; }
 
+    [Required(ErrorMessage = "Name is required!")]
     public string Name { get; set; } = null!;
 
     public virtual ICollection<Book> Books { get; set; } = new List<Book>();
